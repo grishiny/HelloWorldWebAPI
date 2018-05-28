ffada0b3c3be97bdb185f8574fa3efc1fcbc8770
Nageena <nageena.todi@gmail.com>
2018-05-28 01:27:09 -0500

    Add files via upload

---
+++ HelloWorld/HelloWorld.API/Controllers/HelloWorldController.cs
@@ -0,0 +1,38 @@
+﻿using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Web.Http;
+using HelloWorld.Library.Atributes;
+using HelloWorld.Library.Models;
+using HelloWorld.Library.Services;
+
+namespace HelloWorld.API.Controllers
+{
+    public class HelloWorldController : ApiController
+    {
+        /// <summary>
+        ///     The data service
+        /// </summary>
+        private readonly IDataService dataService;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="HelloWorldController" /> class.
+        /// </summary>
+        /// <param name="dataService">The injected data service</param>
+        public HelloWorldController(IDataService dataService)
+        {
+            this.dataService = dataService;
+        }
+
+        /// <summary>
+        ///     Gets hellow world value
+        /// </summary>
+        /// <returns>A HelloWorldData model</returns>
+        [WebApiExceptionFilter(Type = typeof(IOException), Status = HttpStatusCode.ServiceUnavailable, Severity = SeverityCode.Error)]
+        [WebApiExceptionFilter(Type = typeof(SettingsPropertyNotFoundException), Status = HttpStatusCode.ServiceUnavailable, Severity = SeverityCode.Error)]
+        public HelloWorldData Get()
+        {
+            return this.dataService.GetHelloWorldData();
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld.API/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HelloWorld.API.Startup))]
+
+namespace HelloWorld.API
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+        }
+    }
+}
